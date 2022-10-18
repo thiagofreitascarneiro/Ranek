7abf525c31a0faab2fc2d12b80d022ef3b1b187e
Thiago <thiagofreitascarneiro@hotmail.com>
2022-10-18 17:42:18 -0300

    adicionando fotos e estilizando o header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <TheHeader />
     <router-view/>
+    <TheFooter />
   </div>
 </template>
 
+<script>
+
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+  
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body, ul, li, h1, h2, p {
+  padding: 0px;
+  margin: 0 auto;
 }
 
-nav {
-  padding: 30px;
+ul {
+  list-style: none;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  font-family: "Arial", Helvetica, sans-serif;
+  color: #345;
+  background: url('./assets/pattern.svg') repeat top;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <nav>
+            <router-link to="/" class="logo">
+                <img src="@/assets/ranek.svg" alt="Ranek">
+            </router-link>
+            <router-link class="btn" to="/login">
+                Vender / Login
+            </router-link>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "TheHeader"
+}
+</script>
+
+<style>
+
+</style>
