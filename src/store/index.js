3d5815599082d1f082a9fd4bbbd2d4dfcfcb5c83
Thiago <thiagofreitascarneiro@hotmail.com>
2022-11-04 13:48:07 -0300

    add api rank.json

--- src/store/index.js
+++ src/store/index.js
@@ -20,7 +20,6 @@ export default new Vuex.Store({
       cidade: '',
       estado: ''
     },
-    usuario_produtos: null
   },
   getters: {
   },
@@ -30,21 +29,9 @@ export default new Vuex.Store({
     },
     UPDATE_USUARIO(state, payload) {
       state.usuario = Object.assign(state.usuario, payload);
-    },
-    UPDATE_USUARIO_PRODUTOS(state, payload) {
-      state.usuario_produtos = payload;
-    },
-    ADD_USUARIO_PRODUTOS(state, payload) {
-      state.usuario_produtos.unshift(payload);
     }
   },
   actions: {
-    getUsuarioProdutos(context) {
-      return api.get(`/products`)
-        .then(response => {
-          context.commit("UPDATE_USUARIO_PRODUTOS", response.data)
-        })
-    },
     getUsuario(context, payload) {
       return api.get(`/usuario/${payload}`).then(response => {
         context.commit("UPDATE_USUARIO", response.data);
