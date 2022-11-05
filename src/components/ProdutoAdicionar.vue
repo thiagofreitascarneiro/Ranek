6cb2691f3c8d926673f28ae50cef103db692acfe
Thiago <thiagofreitascarneiro@hotmail.com>
2022-11-05 16:45:42 -0300

    produtos adicionar

--- src/components/ProdutoAdicionar.vue
+++ src/components/ProdutoAdicionar.vue
@@ -6,7 +6,7 @@
 
 
 export default {
-    name: "UsuarioProdutos"
+    name: "ProdutoAdicionar"
 }
 </script>
 
--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -5,22 +5,26 @@
         <h2>Seus Produtos</h2>
         <transition-group v-if="usuario_produtos" name="list" tag="ul">
             <li v-for="(produto, index) in usuario_produtos" :key="index">
-                <p>{{produto.descricao}}</p>
+                <ProdutoItem :produto="produto">
+                    <p>{{produto.descricao}}</p>   
+                </ProdutoItem>
             </li>
         </transition-group>
-        {{usuario_produtos}}
+        
     </section>
    
 </template>
 
 <script>
 import ProdutoAdicionar from "@/components/ProdutoAdicionar.vue";
+import ProdutoItem from "@/components/ProdutoItem.vue"
 import { mapState, mapActions } from "vuex";
 
 export default {
     name: "UsuarioProdutos",
     components: {
-        ProdutoAdicionar
+        ProdutoAdicionar,
+        ProdutoItem
     },
     computed: {
         ...mapState(["login", "usuario", "usuario_produtos"])
@@ -41,6 +45,10 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
+h2 {
+    margin-bottom: 20px;
+}
 
 </style>
